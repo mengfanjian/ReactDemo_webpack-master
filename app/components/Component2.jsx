1fba3565bee2a66135e8f2ac3ab01e1278ee4cb9
mengfanjian <mengfanjian@163.com>
2017-03-10 15:15:19 +0800

    Revert "Revert "the react+webpack""
    This reverts commit a9d1177bad962abb6792dfae9393d73a127eebd8.

---
+++ app/components/Component2.jsx
@@ -0,0 +1,13 @@
+//ES6
+import React from 'react';
+
+class Component2 extends React.Component {
+    render() {
+        return (
+            <div>Hello World! by meng </div>
+        )
+    }
+}
+
+//导出组件
+export default Component2;
